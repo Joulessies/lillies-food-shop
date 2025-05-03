067f0af374b51caeda2d96464834c5820e4aca5c
Julius San Jose <112174347+Joulessies@users.noreply.github.com>
2025-05-03 15:50:50 +0800

    fix(*): *

--- django_backend/lillies_backend/settings.py
+++ django_backend/lillies_backend/settings.py
@@ -63,8 +63,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-        'USER': os.environ.get('DB_USERNAME', ''),
-        'PASSWORD': os.environ.get('DB_PASSWORD', ''),
+        'USER': os.environ.get('DB_USERNAME', 'lillies'),
+        'PASSWORD': os.environ.get('DB_PASSWORD', 'lillies01'),
         'HOST': os.environ.get('DB_HOST', 'localhost'),
         'PORT': os.environ.get('DB_PORT', '5432'),
     }
