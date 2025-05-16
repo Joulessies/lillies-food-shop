b51eb6c58db221ed3001369bba82f18567f40967
Julius San Jose <112174347+Joulessies@users.noreply.github.com>
2025-05-16 11:02:20 +0800

    feat: add changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-import { useState } from 'react'
-import './App.css'
+import React from "react";
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -18,8 +19,7 @@ function App() {
         </div>
       </div>
     </>
-  )
+  );
 }
 
-export default App
-
+export default App;
